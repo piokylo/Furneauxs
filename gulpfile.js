751c7378a758fa46c6140f6e8faa5050856f308b
Sean Kiely <seankiely670@gmail.com>
2021-01-19 14:48:54 +0000

    test stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
+const { dest } = require('gulp');
 var gulp = require('gulp');
+var cleanCss = require('gulp-clean-css');
 
-gulp.task('default', async function(){
-    
+gulp.task('css', async function() {
+    return gulp.src('style.css')
+    .pipe(cleanCss())
+    .pipe(gulp.dest('./dest'))
+
+
+
+})
+
+
+gulp.task('watch', async function(){
+    gulp.watch()
 
 
 });
+
+gulp.task('default',['css','watch']);

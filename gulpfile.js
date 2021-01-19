53e98de6d0baed7a184be5688d5298a570054969
Sean Kiely <seankiely670@gmail.com>
2021-01-19 16:27:59 +0000

    js file added

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,16 @@ var gulp = require('gulp');
 var cleanCss = require('gulp-clean-css');
 var browserSync = require('browser-sync');
 
+
 gulp.task('css', function () {
     return gulp.src('style.css')
         .pipe(
             cleanCss({
-
                 compatibility: 'ie8'
+
             })
         )
+
         .pipe(gulp.dest('dist'))
 
 });
@@ -20,6 +22,28 @@ gulp.task('html', function () {
 
 
 });
+
+gulp.task('fonts', function () {
+    return gulp.src('fonts/* ')
+        .pipe(gulp.dest('dist/fonts'))
+
+
+});
+
+gulp.task('img', function () {
+    return gulp.src('img/* ')
+        .pipe(gulp.dest('dist/img'))
+
+
+});
+
+gulp.task('js', function () {
+    return gulp.src('js')
+        .pipe(gulp.dest('dist'))
+
+
+});
+
 
 
 gulp.task('watch', function () {
@@ -34,8 +58,11 @@ gulp.task('watch', function () {
 
     gulp.watch('index.html', gulp.series('html')).on("change", browserSync.reload);
     gulp.watch('style.css', gulp.series('css')).on("change", browserSync.reload);
+    gulp.watch('fonts/*', gulp.series('fonts')).on("change", browserSync.reload);
+    gulp.watch('img/*', gulp.series('img')).on("change", browserSync.reload);
+    gulp.watch('main.js', gulp.series('js')).on("change", browserSync.reload);
 
 });
 
 
-gulp.task('default', gulp.series('html', 'css', 'watch'));
+gulp.task('default', gulp.series('html', 'css','fonts','js', 'img',  'watch'));
